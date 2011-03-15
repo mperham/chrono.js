46b651898d4da9bb5d14f8d4aa01343bb602d7ba
Mike Perham <mperham@gmail.com>
2011-03-15 16:30:29 -0700

    Tabs to spaces

--- chrono.js
+++ chrono.js
@@ -3,7 +3,6 @@
  * Module dependencies.
  */
 var _ = require('underscore')._,
-    backbone = require('backbone'),
     express = require('express'),
     mongodb = require('mongodb'),
     jqtpl = require('jqtpl');
@@ -28,113 +27,113 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-	app.use(express.logger());
+  app.use(express.logger());
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-	app.use(express.logger());
+  app.use(express.logger());
   app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res) {
-	db.open(function(err, ignored) {
-		db.collectionNames(function(err, names) {
-			db.close();
-		  pro =	_(names).chain()
-				.pluck('name')
-				.map(function(x) { return x.substr(x.indexOf('.') + 1); })
-				.reject(function(x) { return (x.indexOf('system.') == 0); })
-				.value();
-			idx = 0;
-		  res.render('index.html.jqtpl', {
-		    locals: {
-		      metrics: pro
-		    }
-		  });
-		});
+  db.open(function(err, ignored) {
+    db.collectionNames(function(err, names) {
+      db.close();
+      pro = _(names).chain()
+        .pluck('name')
+        .map(function(x) { return x.substr(x.indexOf('.') + 1); })
+        .reject(function(x) { return (x.indexOf('system.') == 0); })
+        .value();
+      idx = 0;
+      res.render('index.html.jqtpl', {
+        locals: {
+          metrics: pro
+        }
+      });
+    });
   });
 });
 
 app.post('/metrics', function(req, res) {
-	var hash = req.body;
+  var hash = req.body;
 
-	var doc = { at: new Date(hash.at * 1000), v: hash.v.to_f, ip: req.connection.remoteAddress, k: hash['k'] };
-	db.open(function(err, ignored) {
-		db.collection(doc.k, function(err, coll) {
-			coll.insert(doc, function (err, coll) {
-				db.close();
-			  res.send('', 201);
-			});
-		});
-	});
+  var doc = { at: new Date(hash.at * 1000), v: hash.v.to_f, ip: req.connection.remoteAddress, k: hash['k'] };
+  db.open(function(err, ignored) {
+    db.collection(doc.k, function(err, coll) {
+      coll.insert(doc, function (err, coll) {
+        db.close();
+        res.send('', 201);
+      });
+    });
+  });
 });
 
 
 function query(req, weeks_ago) {
-	var offset = weeks_ago * 60 * 60 * 24 * 7 * 1000;
-	var query = {}
-	if (req.param('start_time', false) || req.param('end_time', false)) {
-		query.at = {} 
-	}
-	if (req.param('start_time', false)) {
-		query.at['$gte'] = new Date(req.param('start_time') * 1000 - offset)
-	}
-	if (req.param('end_time', false)) {
-		query.at['$lt'] = new Date(req.param('end_time') * 1000 - offset)
-	}
-	return query;
+  var offset = weeks_ago * 60 * 60 * 24 * 7 * 1000;
+  var query = {}
+  if (req.param('start_time', false) || req.param('end_time', false)) {
+    query.at = {} 
+  }
+  if (req.param('start_time', false)) {
+    query.at['$gte'] = new Date(req.param('start_time') * 1000 - offset)
+  }
+  if (req.param('end_time', false)) {
+    query.at['$lt'] = new Date(req.param('end_time') * 1000 - offset)
+  }
+  return query;
 }
 
 app.get('/metrics/:name', function(req, res) {
-	db.open(function(err, ignored) {
-		db.collection(req.params.name, function(err, coll) {
-			var prev = parseInt(req.param('previous', 1));
-			var overallResults = [];
-			
-			_.times(prev, function(idx) {
-				coll.find(query(req, idx), { sort: [['at', 1]] }, function (err, cursor) {
-					cursor.toArray(function (err, results) {
-						overallResults[idx] = results;
-						prev -= 1;
-						if (prev == 0) {
-							db.close();
-						  res.send(overallResults, 200);
-						}
-					});
-				});
-			});
+  db.open(function(err, ignored) {
+    db.collection(req.params.name, function(err, coll) {
+      var prev = parseInt(req.param('previous', 1));
+      var overallResults = [];
+      
+      _.times(prev, function(idx) {
+        coll.find(query(req, idx), { sort: [['at', 1]] }, function (err, cursor) {
+          cursor.toArray(function (err, results) {
+            overallResults[idx] = results;
+            prev -= 1;
+            if (prev == 0) {
+              db.close();
+              res.send(overallResults, 200);
+            }
+          });
+        });
+      });
 
-		});
-	});
+    });
+  });
 });
 
 app.get('/load/:name', function(req, res) {
-	db.open(function(err, ignored) {
-		now = new Date().getTime();
-		db.collection(req.params.name, function(err, coll) {
-			coll.remove(function(err, coll) {
-				coll.createIndex('at', function (err, indexName) {
-					var count = 0;
-					var total = 1000;
+  db.open(function(err, ignored) {
+    now = new Date().getTime();
+    db.collection(req.params.name, function(err, coll) {
+      coll.remove(function(err, coll) {
+        coll.createIndex('at', function (err, indexName) {
+          var count = 0;
+          var total = 1000;
 
-					_.times(total, function (idx) {
-						var doc = { at: new Date(now - (idx*60*60*1000)), k: req.params.name, ip: req.connection.remoteAddress, v: (40000 + (Math.random() * 20000)) };
-						coll.insert(doc, function (err, coll) {
-							count += 1;
-							if (count == total) {
-								db.close();
-							  res.send('Loaded ' + total + ' entries', 201);
-							}
-						});
-					});
-					
-				});
-			});
-		});
-	});
+          _.times(total, function (idx) {
+            var doc = { at: new Date(now - (idx*60*60*1000)), k: req.params.name, ip: req.connection.remoteAddress, v: (40000 + (Math.random() * 20000)) };
+            coll.insert(doc, function (err, coll) {
+              count += 1;
+              if (count == total) {
+                db.close();
+                res.send('Loaded ' + total + ' entries', 201);
+              }
+            });
+          });
+          
+        });
+      });
+    });
+  });
 });
 
 // Only listen on $ node app.js
--- test/app.test.js
+++ test/app.test.js
@@ -12,26 +12,26 @@ var models = ['logins', 'searches', 'registrations'];
 
 function removeTestData(models, next) {
   var modelCount = models.length;
-	db.open(function(err, ignored) {
-	  models.forEach(function(modelName) {
-			db.collection(modelName, function(err, collection) {
-				collection.remove(function(err, coll) {
-					coll.createIndex('at', function (err, indexName) {
-						var count = 0;
-						var now = new Date().getTime(); 
+  db.open(function(err, ignored) {
+    models.forEach(function(modelName) {
+      db.collection(modelName, function(err, collection) {
+        collection.remove(function(err, coll) {
+          coll.createIndex('at', function (err, indexName) {
+            var count = 0;
+            var now = new Date().getTime(); 
 
-						for (var idx = 0; idx < 1000; idx++) {
-							var doc = { at: new Date(now - (idx*60*60*1000)), k: modelName, v: (Math.random() * 1000) };
-							coll.insert(doc, function (err, coll) {
-								count += 1;
-								if (count == 1000) {
-									next();
-								}
-							});
-						}
-					});
-				});
-			});
+            for (var idx = 0; idx < 1000; idx++) {
+              var doc = { at: new Date(now - (idx*60*60*1000)), k: modelName, v: (Math.random() * 1000) };
+              coll.insert(doc, function (err, coll) {
+                count += 1;
+                if (count == 1000) {
+                  next();
+                }
+              });
+            }
+          });
+        });
+      });
     });
   });
 }
@@ -43,7 +43,7 @@ function removeTestData(models, next) {
   // Clear tests on each run
   removeTestData(models, function() {
     // Fixtures
-		
+    
     // var user = new app.User({'email' : 'alex@example.com', 'password' : 'test' });
     // user.save(start);
   });
@@ -65,58 +65,58 @@ module.exports = {
       });
   },
 
-	'GET /metrics/logins': function(){
-	    assert.response(app,
-	      { url: '/metrics/logins' },
-	      { status: 200, headers: { 'Content-Type': 'application/json' }},
-	      function(res){
-					var data = JSON.parse(res.body);
-					assert.match(data[0][0].v.toString(), /^[\d\.]+$/);
-	      });
-	  },
+  'GET /metrics/logins': function(){
+      assert.response(app,
+        { url: '/metrics/logins' },
+        { status: 200, headers: { 'Content-Type': 'application/json' }},
+        function(res){
+          var data = JSON.parse(res.body);
+          assert.match(data[0][0].v.toString(), /^[\d\.]+$/);
+        });
+    },
 
-		'GET /metrics/logins with query parameters': function(){
-				var now = parseInt(new Date().getTime() / 1000);
+    'GET /metrics/logins with query parameters': function(){
+        var now = parseInt(new Date().getTime() / 1000);
 
-				var _assert = function(window_count, count) {
-					return function(res) {
-						var data = JSON.parse(res.body);
-//						console.log(data);
-						assert.length(data, window_count);
-						assert.length(data[0], count);
-					};
-				};
-				var _response = { status: 200, headers: { 'Content-Type': 'application/json' }};
+        var _assert = function(window_count, count) {
+          return function(res) {
+            var data = JSON.parse(res.body);
+//            console.log(data);
+            assert.length(data, window_count);
+            assert.length(data[0], count);
+          };
+        };
+        var _response = { status: 200, headers: { 'Content-Type': 'application/json' }};
 
-		    assert.response(app,
-		      { url: '/metrics/logins?start_time=' + (now - 60) + '&end_time=' + (now + 60) },
-		      _response,
-		      _assert(1, 1)
-		      );
+        assert.response(app,
+          { url: '/metrics/logins?start_time=' + (now - 60) + '&end_time=' + (now + 60) },
+          _response,
+          _assert(1, 1)
+          );
 
-		    assert.response(app,
-		      { url: '/metrics/logins?start_time=' + (now - 120) + '&end_time=' + (now - 60) },
-		      _response,
-		      _assert(1, 0)
-					);
+        assert.response(app,
+          { url: '/metrics/logins?start_time=' + (now - 120) + '&end_time=' + (now - 60) },
+          _response,
+          _assert(1, 0)
+          );
 
-	      assert.response(app,
-		      { url: '/metrics/logins?start_time=' + (now + 60) + '&end_time=' + (now + 120) },
-		      _response,
-		      _assert(1, 0)
-					);
+        assert.response(app,
+          { url: '/metrics/logins?start_time=' + (now + 60) + '&end_time=' + (now + 120) },
+          _response,
+          _assert(1, 0)
+          );
 
-			  assert.response(app,
-			    { url: '/metrics/logins?start_time=' + (now - 60) + '&end_time=' + (now + 60) + "&previous=4" },
-			    _response,
-			    _assert(4, 1)
-					);
-		  },
+        assert.response(app,
+          { url: '/metrics/logins?start_time=' + (now - 60) + '&end_time=' + (now + 60) + "&previous=4" },
+          _response,
+          _assert(4, 1)
+          );
+      },
 
-	'POST /metrics': function() {
+  'POST /metrics': function() {
     assert.response(app,
       { url: '/metrics', method: 'POST', headers: { 'content-type': 'application/json' }, data: JSON.stringify({ k: 'logins', v: 4, at: parseInt(Number(new Date())/1000) }) },
-    	{ status: 201, headers: { 'Content-Type': 'text/html; charset=utf8' }, body: '' },
+      { status: 201, headers: { 'Content-Type': 'text/html; charset=utf8' }, body: '' },
       function(res){});
-	},
+  },
 };
